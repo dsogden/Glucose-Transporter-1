fcca9185701a32c16ec498ccb2202d51dfa68fc3
Dylan Ogden <67968474+dsogden@users.noreply.github.com>
2021-12-07 17:08:40 -0600

    Delete salt.sh

--- salt.sh
+++
@@ -1,9 +0,0 @@
-echo "mol new ../glut.psf
-mol addfile ../min.dcd type dcd first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-mol addfile ../trajectory.dcd type dcd first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-" > s.tcl
-
-cat salt.tcl >> s.tcl
-vmd -dispdev text -e s.tcl 2> s.txt
-
-
