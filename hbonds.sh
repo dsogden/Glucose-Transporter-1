d4c03a3a5e1672241f42e8ff7f5b335aa23b9bdc
Dylan Ogden <67968474+dsogden@users.noreply.github.com>
2021-12-08 08:29:23 -0600

    Update hbonds.sh

--- hbonds.sh
+++ hbonds.sh
@@ -9,69 +9,18 @@ source macro.tcl
 package require hbonds
 set N [atomselect top "NTD"]
 set C [atomselect top "CTD"]
+set prot [atomselect top "protein"]
+set lipid [atomselect top "lipid"]
+
 hbonds -sel1 \$N -sel2 \$C -writefile yes -upsel yes -frames all -dist 4.0 -ang 35 \
 -plot no -outdir $i -outfile hbonds-N-C.dat -polar yes -DA both -type all -detailout stdout-N-C
 
-quit
-
-EOF
-
-vmd -dispdev text -e do.tcl
-
-done
-for i in `seq 0 2`
-do
-
-cat > do2.tcl << EOF
-mol new ../glut.psf
-mol addfile ../$i.dcd type dcd first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-source macro.tcl
-
-package require hbonds
-set C [atomselect top "CTD"]
-set ICH [atomselect top "ICHD"]
 hbonds -sel1 \$C -sel2 \$ICH -writefile yes -upsel yes -frames all -dist 4.0 -ang 35 \
 -plot no -outdir $i -outfile hbonds-C-ICH.dat -polar yes -DA both -type all -detailout stdout-C-ICH
 
-quit
-
-EOF
-
-vmd -dispdev text -e do2.tcl
-
-done
-for i in `seq 0 2`
-do
-
-cat > do3.tcl << EOF
-mol new ../glut.psf
-mol addfile ../$i.dcd type dcd first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-source macro.tcl
-
-package require hbonds
-set N [atomselect top "NTD"]
-set ICH [atomselect top "ICHD"]
 hbonds -sel1 \$N -sel2 \$ICH -writefile yes -upsel yes -frames all -dist 4.0 -ang 35 \
 -plot no -outdir $i -outfile hbonds-N-ICH.dat -polar yes -DA both -type all -detailout stdout-N-ICH
 
-quit
-
-EOF
-
-vmd -dispdev text -e do3.tcl
-
-done
-for i in `seq 0 2`
-do
-
-cat > do4.tcl << EOF
-mol new ../glut.psf
-mol addfile ../$i.dcd type dcd first 0 last -1 step 1 filebonds 1 autobonds 1 waitfor all
-source macro.tcl
-
-package require hbonds
-set prot [atomselect top "protein"]
-set lipid [atomselect top "lipid"]
 hbonds -sel1 \$prot -sel2 \$lipid -writefile yes -upsel yes -frames all -dist 4.0 -ang 35 \
 -plot no -outdir $i -outfile hbonds-prot-lipid.dat -polar yes -DA both -type all -detailout stdout-prot-lipid
 
@@ -79,6 +28,6 @@ quit
 
 EOF
 
-vmd -dispdev text -e do4.tcl
+vmd -dispdev text -e do.tcl
 
 done
