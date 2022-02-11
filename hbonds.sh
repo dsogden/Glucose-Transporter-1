b65013cf744ea60491158f285d322fb292460702
Dylan Ogden <67968474+dsogden@users.noreply.github.com>
2022-02-11 14:10:48 -0600

    Update hbonds.sh

--- hbonds.sh
+++ hbonds.sh
@@ -20,19 +20,19 @@ set lipid [atomselect top "lipid"]
 
 # Hbonds between N and C
 hbonds -sel1 \$N -sel2 \$C -writefile yes -upsel yes -frames all -dist 4.0 -ang 35 \
--plot no -outdir $i -outfile hbonds-N-C.dat -polar yes -DA both -type all -detailout stdout-N-C
+-plot no -outdir $i -outfile hbonds-N-C.dat -polar yes -DA both -type unique -detailout stdout-N-C
 
 # Hbonds between C and ICH
 hbonds -sel1 \$C -sel2 \$ICH -writefile yes -upsel yes -frames all -dist 4.0 -ang 35 \
--plot no -outdir $i -outfile hbonds-C-ICH.dat -polar yes -DA both -type all -detailout stdout-C-ICH
+-plot no -outdir $i -outfile hbonds-C-ICH.dat -polar yes -DA both -type unique -detailout stdout-C-ICH
 
 # Hbonds between N and ICH
 hbonds -sel1 \$N -sel2 \$ICH -writefile yes -upsel yes -frames all -dist 4.0 -ang 35 \
--plot no -outdir $i -outfile hbonds-N-ICH.dat -polar yes -DA both -type all -detailout stdout-N-ICH
+-plot no -outdir $i -outfile hbonds-N-ICH.dat -polar yes -DA both -type unique -detailout stdout-N-ICH
 
 # Hbonds betweeen Protein and Lipids
 hbonds -sel1 \$prot -sel2 \$lipid -writefile yes -upsel yes -frames all -dist 4.0 -ang 35 \
--plot no -outdir $i -outfile hbonds-prot-lipid.dat -polar yes -DA both -type all -detailout stdout-prot-lipid
+-plot no -outdir $i -outfile hbonds-prot-lipid.dat -polar yes -DA both -type unique -detailout stdout-prot-lipid
 
 quit
 
